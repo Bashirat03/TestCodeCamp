1eccfd5b0e98103af13c50fae84d6750fdd43b5c
*Bashirat03* <fehintolabashirat@gmail.com>
2023-04-25 13:39:28 +0100

    First java project

---
+++ FirstJavaProject/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // Print the welcome message
+        System.out.printf("Hello and welcome!");
+
+        // Print the message
+        System.out.println("Hello world, this is my first Java project");
+        System.out.println("My favourite food is Amala, i eat it almost everyday and i am going to eat it today.");
+        System.out.println("Lazbat, I will like to have your cat, its very cute. Will you oblige me?");
+        // End the program
+        System.exit(0);
+    }
+}
