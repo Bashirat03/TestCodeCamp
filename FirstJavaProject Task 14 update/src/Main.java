1b839c9fe72183d768aae608623eb5db409faba5
*Bashirat03* <fehintolabashirat@gmail.com>
2023-04-26 01:01:14 +0100

    FirstJavaProject Task 14 update

--- FirstJavaProject Task 14 update/src/Main.java
+++ FirstJavaProject Task 14 update/src/Main.java
@@ -5,8 +5,9 @@ public class Main {
 
         // Print the message
         System.out.println("Hello world, this is my first Java project");
+        System.out.println("Software testing");
         System.out.println("My favourite food is Amala, i eat it almost everyday and i am going to eat it today.");
-        System.out.println("Lazbat, I will like to have your cat, its very cute. Will you oblige me?");
+        System.out.println("Lazbat, I will like to have your cat, its very cute. Will you oblige me? I will be glad if you grant my wish. Thanks");
         // End the program
         System.exit(0);
     }
